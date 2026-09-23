2ccd4dc0c31eba28943fdfef1c443ae24a5846d0
laurenelbert <lael2633@colorado.edu>
2026-09-22 21:56:23 -0600

    i just can't stop winning

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 const rotatingWord = document.querySelector('.accent-word');
 
 if (rotatingWord) {
-  const words = ['beauty', 'convenience', 'love', 'community', 'optimization'];
+  const words = ['beauty', 'convenience', 'love', 'community', 'optimization', 'beans','god','meaning'];
   let wordIndex = 0;
 
   window.setInterval(() => {
